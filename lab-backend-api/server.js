fa6664990eef9197b746f094136ef8bcedad12a4
priyanka0084 <2312084@nec.edu.in>
2025-08-08 15:15:00 +0530

    Added server.js,models,routes

---
+++ lab-backend-api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect('mongodb+srv://priyanka:priya2006@cluster0.qpttesp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(()=>console.log("MongoDB Atlas Connected"))
+.catch(err=>console.log(err));
+const itemRoutes = require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+app.listen(5000,()=>console.log("Server is running on port 5000"));
